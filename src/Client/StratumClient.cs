3bd0414fbdad5d0e122a0e87d6b42bbd78a1f966
Andrew Cann <shum@canndrew.org>
2019-06-08 13:21:05 +0800

    Task 1 complete

--- src/Client/StratumClient.cs
+++ src/Client/StratumClient.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Buffers;
+using System.IO;
 using System.IO.Pipelines;
 using System.Net.Sockets;
 using System.Text;
@@ -19,9 +20,20 @@ namespace TcpEcho {
         public StratumClient (string _endpoint, int _port) : base (_endpoint, _port) { }
 
         public async Task<BlockchainTransactionGet> BlockchainTransactionGet (int id, string transactionId) {
-            var json = "{ \"id\": " + id + ", " +
-                "\"method\": \"blockchain.transaction.get\", " +
-                "\"params\": [\"" + transactionId + "\"] }";
+            var stringBuilder = new StringBuilder();
+            var stringWriter = new StringWriter(stringBuilder);
+            var jsonWriter = new JsonTextWriter(stringWriter);
+            jsonWriter.WriteStartObject();
+            jsonWriter.WritePropertyName("id");
+            jsonWriter.WriteValue(id);
+            jsonWriter.WritePropertyName("method");
+            jsonWriter.WriteValue("blockchain.transaction.get");
+            jsonWriter.WritePropertyName("params");
+            jsonWriter.WriteStartArray();
+            jsonWriter.WriteValue(transactionId);
+            jsonWriter.WriteEnd();
+            jsonWriter.WriteEndObject();
+            var json = stringBuilder.ToString();
             var returnedJson = await Call (json);
             var obj = JsonConvert.DeserializeObject<BlockchainTransactionGet> (returnedJson);
             return obj;
