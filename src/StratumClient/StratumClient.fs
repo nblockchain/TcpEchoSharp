20e9818e8007b7d7c8e9621d0145f090f145f850
Andrew Cann <shum@canndrew.org>
2019-06-12 14:01:25 +0800

    fixes based on feedback

--- src/StratumClient/StratumClient.fs
+++ src/StratumClient/StratumClient.fs
@@ -6,65 +6,50 @@ open Newtonsoft.Json
 
 type BlockchainTransactionGet = {
     [<JsonProperty(PropertyName = "id")>]
-    mutable Id: int
+    Id: int
     [<JsonProperty(PropertyName = "result")>]
-    mutable Result: string
+    Result: string
 }
 
 type BlockchainEstimateFee = {
     [<JsonProperty(PropertyName = "id")>]
-    mutable Id: int
+    Id: int
     [<JsonProperty(PropertyName = "result")>]
-    mutable Result: double
+    Result: double
+}
+
+type Request = {
+    [<JsonProperty(PropertyName = "id")>]
+    Id: int
+    [<JsonProperty(PropertyName = "method")>]
+    Method: string
+    [<JsonProperty(PropertyName = "params")>]
+    Params: seq<obj>
 }
 
 type StratumClient(endpoint: string, port: int) =
     inherit Client(endpoint, port)
     member this.Call(json: string) = base.Call(json)
     member this.BlockchainTransactionGet(id: int, transactionId: string) = Async.StartAsTask(async {
-        let stringBuilder = new StringBuilder()
-        let stringWriter = new StringWriter(stringBuilder)
-        let jsonWriter = new JsonTextWriter(stringWriter)
-
-        // Is there a more elegant way to do this?
-        // In Rust you could derive(Serialize) on a type and have this code
-        // generated. Being able to use the |> operator (or something similar)
-        // would also be nice.
-        jsonWriter.WriteStartObject()
-        jsonWriter.WritePropertyName("id")
-        jsonWriter.WriteValue(id)
-        jsonWriter.WritePropertyName("method")
-        jsonWriter.WriteValue("blockchain.transaction.get")
-        jsonWriter.WritePropertyName("params")
-        jsonWriter.WriteStartArray()
-        jsonWriter.WriteValue(transactionId)
-        jsonWriter.WriteEnd()
-        jsonWriter.WriteEndObject()
-
-        let json = stringBuilder.ToString()
-        let! returnedJson = this.Call(json) |> Async.AwaitTask
-        let obj = JsonConvert.DeserializeObject<BlockchainTransactionGet>(returnedJson)
+        let request: Request = {
+            Id = id
+            Method = "blockchain.transaction.get"
+            Params = [transactionId]
+        }
+        let json = JsonConvert.SerializeObject(request)
+        let! returnedJson = this.Call json |> Async.AwaitTask
+        let obj = JsonConvert.DeserializeObject<BlockchainTransactionGet> returnedJson
         return obj
     })
     member this.BlockchainEstimateFee(id: int, blocks: int) = Async.StartAsTask(async {
-        let stringBuilder = new StringBuilder()
-        let stringWriter = new StringWriter(stringBuilder)
-        let jsonWriter = new JsonTextWriter(stringWriter)
-
-        jsonWriter.WriteStartObject()
-        jsonWriter.WritePropertyName("id")
-        jsonWriter.WriteValue(id)
-        jsonWriter.WritePropertyName("method")
-        jsonWriter.WriteValue("blockchain.estimatefee")
-        jsonWriter.WritePropertyName("params")
-        jsonWriter.WriteStartArray()
-        jsonWriter.WriteValue(blocks)
-        jsonWriter.WriteEnd()
-        jsonWriter.WriteEndObject()
-
-        let json = stringBuilder.ToString()
-        let! returnedJson = this.Call(json) |> Async.AwaitTask
-        let obj = JsonConvert.DeserializeObject<BlockchainEstimateFee>(returnedJson)
+        let request: Request = {
+            Id = id
+            Method = "blockchain.estimatefee"
+            Params = [blocks]
+        }
+        let json = JsonConvert.SerializeObject(request)
+        let! returnedJson = this.Call json |> Async.AwaitTask
+        let obj = JsonConvert.DeserializeObject<BlockchainEstimateFee> returnedJson
         return obj
     })
 
