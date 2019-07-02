de108990962e4f8f6e572235c62884506fe295dc
Kanaruna <>
2019-07-02 10:40:35 +0200

    Fixing issues.

--- make.sh
+++ make.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
 #
 
-PATH1=`pwd`
-echo $PATH1;
-find . -maxdepth 1 -type f -iname \*.sln > tmp1.out
-COUNT=`cat tmp1.out | wc -l`
+PROJECT=`find . -maxdepth 1 -type f -iname \*.sln -exec basename {} \;`
 
-if [ $COUNT = 1 ]; then
-while read i; do
-PROJECT=$i;
-echo $PROJECT;
-done < tmp1.out
-        else
-            	if [ $COUNT = 0 ]; then
-                                echo "No .sln found"
-                        exit 1
-                fi
-        echo "ERROR: too many .sln files";
-        exit 1
-fi
 echo "Restoring packages..."
 msbuild -t:restore $PROJECT
 echo "Building source..."
-msbuild -t:build $PROJECT | tee tmp2.out
+msbuild -t:build $PROJECT 
 
 check()
 {
-echo "Running net461 tests..."
-find . -regextype sed -regex  ".*/\net461\/ClientTests.dll$" -exec nunit-console {} \;
+    echo "Running net461 tests..."
+    find . -regextype sed -regex  ".*/\net461\/ClientTests.dll$" -exec nunit-console {} \;
 }
 
 "$@"
