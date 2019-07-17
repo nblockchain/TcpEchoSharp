7cd3d49915affd41167eaa71eb65308e89bfa7b8
Lukas Nordin <lukasnordin11@gmail.com>
2019-07-17 20:27:03 +0200

    Downgrade mono to last successful build

--- install_mono_from_microsoft_deb_packages.sh
+++ install_mono_from_microsoft_deb_packages.sh
@@ -10,5 +10,5 @@ apt install -y ca-certificates
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
 apt update
-apt install -y mono-devel fsharp
+apt install -y mono-devel=5.20.1.19 fsharp
 mono --version
